89c702becc398e2e16da281940f7780707b4cd66
sebastianschulz <deplayer86@web.de>
2012-07-07 23:04:00 +0200

    join/leave works
    including gui update after joining/leaving, manual update from
    webservice works as well!

--- src/tawusel/android/ui/TourActivity.java
+++ src/tawusel/android/ui/TourActivity.java
@@ -14,8 +14,10 @@ import tawusel.android.tools.config.PropertyManager;
 import tawusel.android.ui.helper.JSONArrayHelper;
 import tawusel.android.ui.helper.Time;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,6 +25,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TableRow.LayoutParams;
@@ -47,7 +50,7 @@ public class TourActivity extends Activity {
         //get the tableLayout object
         tblTours = (TableLayout) findViewById(R.id.tours_tourTable);
         
-        createTourRows();
+        updateTourRows(true);
     }
 	
     @Override
@@ -57,7 +60,7 @@ public class TourActivity extends Activity {
             resumeHasRun = true;
             return;
         }
-    	updateTourRows();
+    	updateTourRows(false);
     }
     
 	@Override
@@ -75,10 +78,7 @@ public class TourActivity extends Activity {
 	            createNewTour();
 	            return true;
 	        case R.id.menu_update:
-	        	db.openDatabase();
-	        	db.clearTemplateTable();
-	        	db.closeDatabase();
-	        	createTourRows();
+	        	performManualUpdate();
 	        	return true;
 	        case R.id.menu_help:
 	            showHelp();
@@ -91,16 +91,31 @@ public class TourActivity extends Activity {
 	    }
 	}
 
-	private void createTourRows() {
-		updateTourDataFromWebService();
-		updateTourRows();
+	private void performManualUpdate() {
+		db.openDatabase();
+		db.clearTemplateTable();
+		db.closeDatabase();
+		updateTourRows(true);
+	}
+
+	public void updateTourRows(boolean updateLocalDbFromService) {
+		if (updateLocalDbFromService) {
+			updateTourDataFromWebService();
+		}
+		clearTourRows();
+		createTourViews();
 	}
 	
-	private void updateTourRows() {
+	private void clearTourRows() {
+		for (int i = tblTours.getChildCount(); i > 0; i--) {
+			tblTours.removeView(tblTours.getChildAt(i));
+		}
+	}
+
+	private void createTourViews() {
 		db.openDatabase();
 		Vector<String[]> tours = db.getAllTours();
 		db.closeDatabase();
-		
 		for (String[] tour : tours) {
 			addTourRow(tour);
 		}
@@ -287,4 +302,203 @@ public class TourActivity extends Activity {
 		super.onDestroy();
 	}
 	
+	private class TourDetailsDialog extends Dialog implements OnClickListener, OnDismissListener {
+		Database db;
+		
+		private TextView tvCity, tvFrom, tvTo, tvTime, tvStatus, tvPassengers;
+		private Button bJoin, bLeave;
+		
+		private String[] tour = new String[10];
+		private String[] status = new String[3];
+		private Vector<String> user;
+		private Context context;
+		
+		@SuppressWarnings("unchecked")
+		public TourDetailsDialog(Context context, String[] tour, String[] status, Vector<String> user) {
+			super(context);
+			
+			this.setContentView(R.layout.tour_details_dialog);
+	 	   	this.setTitle(R.string.tourDetailsDialog_head);
+	 	   	
+	 	   	this.tour = tour;
+	 	   	this.status = status;
+	 	   	this.user = (Vector<String>) user.clone();
+	 	   	this.context = context;
+	 	   	this.db = new Database(context);
+	 	   	
+	 	   	initGuiElements();
+	 	   	updateDialog();
+		}
+		
+		private void initGuiElements() {
+			tvCity = (TextView) findViewById(R.id.tourDetailsDialog_tvCity);
+			tvFrom = (TextView) findViewById(R.id.tourDetailsDialog_tvFrom);
+			tvTo = (TextView) findViewById(R.id.tourDetailsDialog_tvTo);
+			tvTime = (TextView) findViewById(R.id.tourDetailsDialog_tvTime);
+			tvStatus = (TextView) findViewById(R.id.tourDetailsDialog_tvStatus);
+			tvPassengers = (TextView) findViewById(R.id.tourDetailsDialog_tvPassengers);
+			bJoin = (Button) findViewById(R.id.tourDetailsDialog_bJoinButton);
+			bJoin.setOnClickListener(this);
+			bLeave = (Button) findViewById(R.id.tourDetailsDialog_bLeaveButton);
+			bLeave.setOnClickListener(this);
+		}
+		
+		private void updateDialog() {
+			setTextFieldValues();
+			disableButton();
+		}
+		
+		private void setTextFieldValues() {
+			tvCity.setText(tour[1]);
+			tvFrom.setText(tour[2]);
+			tvTo.setText(tour[3]);
+			TourKind tourKind = TourKind.parseTourKind(tour[9]);
+			tvTime.setText(Time.formatString(tour[4], tour[5], tourKind));
+			tvStatus.setText(status[1] + " - " + status[2]);
+			tvPassengers.setText(getPassengers(tour[7]));
+		}
+		
+		private String getPassengers(String jsonArrayString) {
+			String passengers = "";
+			try {
+				JSONArray jsonPassengers = JSONCommunicator.parseJSArray(jsonArrayString);
+				
+				for (int i = 0; i < jsonPassengers.length(); i++) {
+					JSONObject passengersObject = jsonPassengers.getJSONObject(i);
+					JSONArray passengersNameArray = passengersObject.names();
+					JSONArray passengersValArray = passengersObject.toJSONArray(passengersNameArray);
+					
+					//need to sort the json arrays because they are not build in the way they are sended
+					Vector<JSONArray> arrays = new Vector<JSONArray>();
+					arrays.add(passengersNameArray);
+					arrays.add(passengersValArray);
+					arrays = JSONArrayHelper.sortPassengersArray(arrays);
+					passengersValArray = arrays.get(1);
+					
+					if(i!=0) {
+						passengers += ", ";
+					}
+					passengers += passengersValArray.getString(2);
+				}
+				return passengers;
+			} catch (Exception e) {
+				return passengers;
+			}
+		}
+		   
+		private void disableButton() {
+			//check if emailstring of logged in user is contained in pessengers
+			boolean isUserPassengerOfTour = tour[7].contains(user.get(0));
+			if(!user.isEmpty() && isUserPassengerOfTour) {
+				bJoin.setEnabled(false);
+				bLeave.setEnabled(true);
+			} else if(!isUserPassengerOfTour) {
+				bLeave.setEnabled(false);
+				bJoin.setEnabled(true);
+			}
+		}
+
+		public void onClick(View v) {
+			if(v == bJoin) {
+				boolean isJoined = tryToJoinTour();
+				if(isJoined) {
+					updateDialog();
+					TourActivity.this.updateTourRows(false);
+					Toast.makeText(context, R.string.tourDetailsDialog_userJoined, Toast.LENGTH_LONG).show();
+				}
+			} else if(v == bLeave) {
+				boolean isLeft = tryToLeaveTour();
+				if(isLeft) {
+					updateDialog();
+					TourActivity.this.updateTourRows(false);
+					Toast.makeText(context, R.string.tourDetailsDialog_userLeft, Toast.LENGTH_LONG).show();
+				}
+			}
+		}
+		
+		private boolean tryToJoinTour() {
+			String params = URLEncoder.encode(user.get(0)) + "/" + tour[0];
+			try {
+				JSONArray jsonResult = JSONCommunicator.getJSONArray("joinTourByApp/", params, PropertyManager.getJSONServer());
+				
+				if(jsonResult != null) {
+					JSONObject result = jsonResult.getJSONObject(0);
+					JSONArray resultNameArray = result.names();
+					JSONArray resultValArray = result.toJSONArray(resultNameArray);
+					
+					//need to sort the json arrays because they are not build in the way they are sended
+					Vector<JSONArray> arrays = new Vector<JSONArray>();
+					arrays.add(resultNameArray);
+					arrays.add(resultValArray);
+					arrays = JSONArrayHelper.sort(arrays);
+					resultValArray = arrays.get(1);
+					
+					if(!resultValArray.get(0).toString().equals(false)) {
+						for (int j = 0; j < resultValArray.length(); j++) {
+							tour[j] = resultValArray.get(j).toString();;
+						}
+						tour[9] = TourKind.ACTIVE.toString();
+						
+						db.openDatabase();
+						db.updateTour(tour);
+						db.closeDatabase();
+						
+						return true;
+					}
+				}	
+				return false;
+			} catch (Exception e) {
+				ErrorDialog errorDialog = new ErrorDialog(context, "Unable to join the tour", e.toString() + " - " +e.getMessage());
+				errorDialog.show();
+				return false;
+			}
+		}
+
+		private boolean tryToLeaveTour() {
+			String params = URLEncoder.encode(user.get(0)) + "/" + tour[0];
+			try {
+				JSONArray jsonResult = JSONCommunicator.getJSONArray("leaveTourByApp/", params, PropertyManager.getJSONServer());
+				
+				if(jsonResult != null) {
+					JSONObject result = jsonResult.getJSONObject(0);
+					JSONArray resultNameArray = result.names();
+					JSONArray resultValArray = result.toJSONArray(resultNameArray);
+					
+					//need to sort the json arrays because they are not build in the way they are sended
+					Vector<JSONArray> arrays = new Vector<JSONArray>();
+					arrays.add(resultNameArray);
+					arrays.add(resultValArray);
+					arrays = JSONArrayHelper.sort(arrays);
+					resultValArray = arrays.get(1);
+					
+					if(!resultValArray.get(0).toString().equals(false)) {
+						for (int j = 0; j < resultValArray.length(); j++) {
+							tour[j] = resultValArray.get(j).toString();;
+						}
+						tour[9] = TourKind.AVAILABLE.toString();
+						
+						
+						db.openDatabase();
+						db.updateTour(tour);
+						db.closeDatabase();
+						
+						return true;
+					}
+				}	
+				return false;
+			} catch (Exception e) {
+				ErrorDialog errorDialog = new ErrorDialog(context, "Unable to leave the tour", e.toString() + " - " +e.getMessage());
+				errorDialog.show();
+				return false;
+			}
+		}
+
+		public void onDismiss(DialogInterface dialog) {
+			TourActivity.this.updateTourRows(false);
+		}
+	}
+
+	
 }
+
+
